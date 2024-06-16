058513dd43a7d196bc5fd92d481b84a9ea6cee65
talktoash <talktoashab@gmail.com>
2024-06-16 12:21:13 +0530

    here is the code

--- demo.js
+++ demo.js
@@ -1,1 +1,14 @@
-console.log("practice");
+let mainHeading=document.getElementById('main-heading');
+mainHeading.textContent='Fruit World';
+mainHeading.style.color='orange';
+
+let header=document.getElementById('header');
+header.style.backgroundColor='green';
+
+header.style.borderBottom='solid orange';
+
+let basketHeading=document.getElementById('basket-heading');
+basketHeading.style.color='green';
+
+let thanks=document.getElementById('thanks');
+thanks.innerHTML='<p>Please visit us again</p>'
